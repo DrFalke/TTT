35d0b5a9e49483df1f3169c8b8910c078eb80538
Florian falkner <162137350+DrFalke@users.noreply.github.com>
2024-05-21 13:22:15 +0200

    Server_v1

--- Server_test.py
+++ Server_test.py
@@ -1,3 +1,26 @@
 import socket
 
-print("Server is running...")
+import socket
+
+HOST = "10.10.218.64"
+PORT = 8080
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.bind((HOST, PORT))
+server.listen(1)
+
+print("Server läuft und wartet auf Verbindung")
+
+client, address = server.accept()
+print(f"Es konnte mit {address} eine Verbindung aufgebaut werden")
+
+while True:
+    message = client.recv(1024).decode()
+    if message == "exit":
+        break
+    print(f"Client: {message}")
+    reply = input("Server: ")
+    client.send(reply.encode())
+
+client.close()
+server.close()
