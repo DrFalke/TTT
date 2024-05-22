676cff8b664855c656342c1cc42796119c421c71
Florian Lindner <168716133+fllindnerHTL@users.noreply.github.com>
2024-05-22 17:53:51 +0200

    Update Client_test.py

--- Client_test.py
+++ Client_test.py
@@ -1,24 +1,33 @@
 import socket
-# define the ipaddress and the port number
-host = '10.10.218.64'
-port = 8080
-# create a socket at client side using TCP/IP protocol
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host , port))
-print("Connected to the server")
-# receive data from the server
-while True:
-    # Receive data from the server
-    reply = client.recv(1024).decode()
+import threading
 
-    #just for testing 
-    # Print the received data (playground)
-    print("Playground:")
-    print(reply)
-    # Ask the user to input the x and y coordinates
-    x = int(input("input x-cord ->"))
-    y = int(input("input y-cord ->"))
-    # Send the coordinates to the server
-    message = f"{x},{y}"
-    client.send(message.encode())
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            client_socket.close()
+            break
 
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = input("Bitte geben Sie die Server-IP ein: "), 8080
+    client_socket.connect(server_address)
+
+    username = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+    client_socket.send(username.encode('utf-8'))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
+
+    while True:
+        message = input()
+        if message == "/exit":
+            client_socket.send(message.encode('utf-8'))
+            client_socket.close()
+            break
+        else:
+            client_socket.send(message.encode('utf-8'))
+
+start_client()
