85030568a1b9e12eb7a53f0c56f015612cba437b
Florian falkner <162137350+DrFalke@users.noreply.github.com>
2024-05-23 00:29:25 +0200

    update

--- TTT_Client.py
+++ TTT_Client.py
@@ -1,14 +1,9 @@
-#Connection
 import socket
 
 HOST = "127.0.0.1" #localhost
 PORT = 61111 #port number
-
-#IPv4 TCP/IP socket is created and bound to the address and port
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(1) #a maximum of 1 connection is allowed
-conn, addr = server.accept() #accept connection
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4 TCP/IP socket is created
+server.connect((HOST, PORT)) #connect to the server
 
 x_dim = 3 #x dimension
 y_dim = 3 #y dimension
