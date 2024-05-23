ccf9136a2d583f1166aa4b2672258eaa533c9ef5
Florian falkner <162137350+DrFalke@users.noreply.github.com>
2024-05-23 11:12:56 +0200

    versuch verworfen

--- TTT_Server.py
+++ TTT_Server.py
@@ -12,10 +12,7 @@ conn, addr = server.accept() #accept the connection
 x_dim = 3 #x dimension of the board 
 y_dim = 3 #y dimension of the board
 playground_list = [[" "]*y_dim for i in range(x_dim)] #playground list
-round = 1 #round counter 
-
-player1 = "X" #starting player
-player2 = "O" #second player
+round = 1 #round counter   
 #playground function
 def playground():
     for j in range(x_dim):
@@ -25,8 +22,35 @@ def playground():
         for x in range(x_dim):
             print("|{}".format(playground_list[x][y]),end="")
         print("|",y)
-#make move function
-def make_move():
+
+#check for win
+def check_for_win():
+    # Check for horizontal win
+    for x in range(0, 3):
+        if playground_list[x][0] == playground_list[x][1] == playground_list[x][2] and playground_list[x][0] != ' ':
+            return True
+    # Check for vertical win
+    for y in range(0, 3):
+        if playground_list[0][y] == playground_list[1][y] == playground_list[2][y] and playground_list[0][y] != ' ':
+            return True
+    # Check for diagonal win
+    if playground_list[0][0] == playground_list[1][1] == playground_list[2][2] and playground_list[0][0] != ' ':
+        return True
+    if playground_list[0][2] == playground_list[1][1] == playground_list[2][0] and playground_list[0][2] != ' ':
+        return True
+    else:
+        return False
+#check for draw
+def check_for_draw():
+    for x in range(0, 3):
+        for y in range(0, 3):
+            if playground_list[x][y] == " ":
+                return False
+    return True
+
+#game loop
+while (round <= 9):
+    playground()
     if (round % 2 == 0):
         player = "X"
         print("it's player {} turn!".format(player))
@@ -67,32 +91,3 @@ def make_move():
             print("Fild already taken!")
     else:
         print("The input is out of range!")
-#check for win
-def check_for_win():
-    # Check for horizontal win
-    for x in range(0, 3):
-        if playground_list[x][0] == playground_list[x][1] == playground_list[x][2] and playground_list[x][0] != ' ':
-            return True
-    # Check for vertical win
-    for y in range(0, 3):
-        if playground_list[0][y] == playground_list[1][y] == playground_list[2][y] and playground_list[0][y] != ' ':
-            return True
-    # Check for diagonal win
-    if playground_list[0][0] == playground_list[1][1] == playground_list[2][2] and playground_list[0][0] != ' ':
-        return True
-    if playground_list[0][2] == playground_list[1][1] == playground_list[2][0] and playground_list[0][2] != ' ':
-        return True
-    else:
-        return False
-#check for draw
-def check_for_draw():
-    for x in range(0, 3):
-        for y in range(0, 3):
-            if playground_list[x][y] == " ":
-                return False
-    return True
-
-#game loop
-while (round <= 9):
-    playground()
-    make_move()
