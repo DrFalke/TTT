d7e420c680ed9e65fb86e77ad6174a0091ec9199
Florian Lindner <168716133+fllindnerHTL@users.noreply.github.com>
2024-06-02 18:40:18 +0200

    Update zum Server: Server funktioniert und hier wird die gesamte Spiellogik verwaltet.

--- Server_dummer_Client.py
+++ Server_dummer_Client.py
@@ -1,94 +1,71 @@
+"""Der Server berechnet sämtliche Züge und gibt das Ergebnis an den Client zurück. 
+Der Client ist nur für die Darstellung zuständig. Der Server ist der "Schlaue" und der Client der "Dumme".
+"""
+
 import socket
 import pickle
-import tkinter as tk
-from tkinter import messagebox
+import threading
 
 class TicTacToeServer:
-    def __init__(self, host='localhost', port=12345):
+    def __init__(self, host='192.168.56.1', port=8080):
         self.host = host
         self.port = port
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
         self.current_player = 'X'
-        self.create_gui()
-
-    def create_gui(self):
-        self.root = tk.Tk()
-        self.root.title('Tic-Tac-Toe Server')
-        self.buttons = [[None for _ in range(3)] for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                self.buttons[i][j] = tk.Button(self.root, text=' ', font=('normal', 40), width=5, height=2,
-                                               command=lambda i=i, j=j: self.on_button_click(i, j))
-                self.buttons[i][j].grid(row=i, column=j)
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-
-    def on_button_click(self, row, col):
-        if self.board[row][col] == ' ' and self.current_player == 'X':
-            self.board[row][col] = self.current_player
-            self.buttons[row][col].config(text=self.current_player)
-            self.current_player = 'O'
-            self.send_board()
-            if self.check_winner():
-                messagebox.showinfo("Game Over", f"Player {self.board[row][col]} wins!")
-                self.reset_board()
-            self.wait_for_move()
-
-    def reset_board(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                self.buttons[i][j].config(text=' ')
-        self.current_player = 'X'
-        self.send_board()
-
-    def check_winner(self):
-        lines = (
-            [self.board[0], self.board[1], self.board[2]],
-            [list(col) for col in zip(*self.board)],
-            [[self.board[i][i] for i in range(3)], [self.board[i][2 - i] for i in range(3)]]
-        )
-        for line_set in lines:
-            for line in line_set:
-                if line.count(line[0]) == 3 and line[0] != ' ':
-                    return line[0]
-        return None
+        self.players = []
+        self.start_server()
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen()
-        print(f'Server listening on {self.host}:{self.port}')
-        self.conn, addr = self.server_socket.accept()
-        print(f'Connected by {addr}')
-        self.send_board()
+        self.server_socket.listen(2)
+        print("Server started. Waiting for players...")
+        while len(self.players) < 2:
+            client_socket, addr = self.server_socket.accept()
+            self.players.append(client_socket)
+            print(f"Player connected from {addr}")
+            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
-    def send_board(self):
-        data = pickle.dumps(self.board)
-        self.conn.sendall(data)
-        
-    def wait_for_move(self):
+    def handle_client(self, client):
         while True:
-            data = self.sock.recv(4096)
-            move = pickle.loads(data)
-            if move is not None:
-                if len(move) == 2:
-                    row, col = move
-                    if self.board[row][col] == ' ':
-                        self.board[row][col] = 'X'
-                        self.update_board()
-                        if self.check_winner():
-                            messagebox.showinfo("Game Over", "Player X wins!")
-                            self.reset_board()
+            data = client.recv(1024)
+            row, col = pickle.loads(data)
+            if self.make_move(row, col):
+                print(f"Move made at ({row}, {col}) by {self.current_player}")
+                for player in self.players:
+                    player.send(pickle.dumps((self.board, self.current_player)))
+                if self.check_winner():
+                    print(f"Player {self.current_player} wins!")
+                    break
+                elif self.is_board_full():
+                    print("The game is a draw!")
+                    break
+        client.close()
 
-def on_closing(self):
-    self.conn.close()
-    self.server_socket.close()
-    self.root.destroy()
+    def make_move(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            self.current_player = 'O' if self.current_player == 'X' else 'X'
+            return True
+        return False
 
-    def start(self):
-        self.root.after(100, self.start_server)
-        self.root.mainloop()
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
+                return True
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+                return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return True
+        return False
 
-if __name__ == '__main__':
+    def is_board_full(self):
+        for row in self.board:
+            if ' ' in row:
+                return False
+        return True
+
+if __name__ == "__main__":
     server = TicTacToeServer()
-    server.start()
